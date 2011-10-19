8ab16e51f23861204c285968aa9fa38201bb97ec
deltam <deltam@gmail.com>
2011-10-20 03:43:30 +0900

    added test tool epubcheck-1.2

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject clj-epub "0.1.0"
+(defproject clj-epub "0.1.1"
   :description "Library for generate EPUB, on Clojure"
   :url "http://github.com/deltam/clj-epub"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [hiccup "0.3.0"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dev-dependencies [[org.clojars.deltam/epubcheck "1.2"]
+                     [swank-clojure "1.2.1"]
+                     [lein-clojars "0.7.0"]]
   :repositories {"markdownj" "http://scala-tools.org/repo-releases"})
